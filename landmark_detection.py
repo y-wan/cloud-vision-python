472294419c2f8a9a8e60309a8ce12ce71c17d543
y-wan <908244927@qq.com>
2017-04-02 23:35:07 +0800

    Add landmark_detection.py

---
+++ landmark_detection.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+from google.cloud import vision
+client = vision.Client()
+image = client.image(source_uri='gs://gracewan-images-bucket/selfie.png')
+landmarks = image.detect_landmarks(limit=5)
+for i, landmark in enumerate(landmarks):
+    print('Landmark {0}:'.format(i+1))
+    print(landmark.description+'\n'+str(landmark.locations[0].latitude)+'\n'+str(landmark.locations[0].longitude)+'\n'+str(landmark.score)+'\n')
